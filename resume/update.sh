92d34eea1aa17ba7902eb44372346d72b5c7b086
Gerry Chen <gerry.chen2015@gmail.com>
2022-02-08 12:45:55 -0500

    reformat "about" page

--- resume/update.sh
+++ resume/update.sh
@@ -3,3 +3,4 @@
 cp output/resume.pdf GerryChen_resume.pdf
 cp output/resume.pdf ../GerryChen_resume.pdf
 cp -r ../resume/* ~/GIT_REPOS/gerrysworld2/resume/
+convert ~/GIT_REPOS/gerrysworld2/resume/GerryChen_resume.pdf[0] ~/GIT_REPOS/gerrysworld2/resume/CV.png
