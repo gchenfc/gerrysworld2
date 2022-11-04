bb9604bbe39f5eccf10101fd133508588dc0b62f
Gerry Chen <gerry.chen2015@gmail.com>
2022-11-04 02:26:58 -0400

    update url filter to also allow links relative to site root

--- _plugins/url_filter.rb
+++ _plugins/url_filter.rb
@@ -1,6 +1,6 @@
 module UrlFilter
   def prepend_project_path_if_relative(input, project)
-    (input.include? "://") ? input : prepend_project_path(input, project)
+    (input.include?"://" or input.start_with?"/") ? input : prepend_project_path(input, project)
   end
   def prepend_project_path(input, project)
     base = remove(project.url, "/index.html")
