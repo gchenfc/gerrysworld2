ebc81f81dc59eaa8d7dd7fd888cfde5828d87a6b
Gerry Chen <gerry.chen2015@gmail.com>
2022-11-04 02:49:32 -0400

    update CV photo ad remove unused files

--- CV/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp output/CV.pdf GerryChen_CV.pdf
-cp output/CV.pdf ../GerryChen_CV.pdf
-cp -r ../CV/* ~/GIT_REPOS/gerrysworld2/CV/
-convert ~/GIT_REPOS/gerrysworld2/CV/GerryChen_CV.pdf[0] ~/GIT_REPOS/gerrysworld2/CV/CV.png
