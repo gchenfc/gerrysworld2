1c7aa3a6c46b19924b190d2b1b9dda44140a1c26
Gerry Chen <gerry.chen2015@gmail.com>
2022-11-04 00:40:14 -0400

    custom ruby filter to handle rel/abs image urls better

---
+++ _plugins/url_filter.rb
@@ -0,0 +1,11 @@
+module UrlFilter
+  def prepend_project_path_if_relative(input, project)
+    (input.include? "://") ? input : prepend_project_path(input, project)
+  end
+  def prepend_project_path(input, project)
+    base = remove(project.url, "/index.html")
+    "#{base}/#{input}"
+  end
+end
+
+Liquid::Template.register_filter(UrlFilter)
