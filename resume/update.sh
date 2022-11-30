944ad9f303c1e9032a6fd3683cdf2a184821e93c
Gerry Chen <gerry.chen2015@gmail.com>
2022-11-30 15:27:36 -0500

    update resume

--- resume/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp output/resume.pdf GerryChen_resume.pdf
-cp output/resume.pdf ../GerryChen_resume.pdf
-cp -r ../resume/* ~/GIT_REPOS/gerrysworld2/resume/
-convert ~/GIT_REPOS/gerrysworld2/resume/GerryChen_resume.pdf[0] ~/GIT_REPOS/gerrysworld2/resume/CV.png
